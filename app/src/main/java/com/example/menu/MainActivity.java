8a400a8cb4cffd303d9255481ace791e9c3f3d5d
Таисия Ходарева <taisiya.khodareva@gmail.com>
2025-01-16 00:22:47 +0600

    first commit

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        TextView infoTextView = findViewById(R.id.textView);
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_cat1:
+                infoTextView.setText("Вы выбрали кота!");
+                return true;
+            case R.id.action_cat2:
+                infoTextView.setText("Вы выбрали кошку!");
+                return true;
+            case R.id.action_cat3:
+                infoTextView.setText("Вы выбрали котёнка!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onSettingsMenuClick(MenuItem item){
+        TextView infoTextView = findViewById(R.id.textView);
+        infoTextView.setText("Вы выбрали пункт Setting, лучше бы выбрали кота!");
+    }
+}
