6847d95074f63e2b390eb4249bb4eb25f901b8c6
Таисия Ходарева <taisiya.khodareva@gmail.com>
2025-01-16 00:18:54 +0600

    first commit

---
+++ app/src/main/java/com/example/hellokitty/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hellokitty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
